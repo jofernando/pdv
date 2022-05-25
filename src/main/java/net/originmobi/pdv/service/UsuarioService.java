e03b3f80f08fd537baa28fa2e3df40d83c9b137a
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-05-25 15:45:22 -0300

    autowired no construtor, remove linhas em branco

--- src/main/java/net/originmobi/pdv/service/UsuarioService.java
+++ src/main/java/net/originmobi/pdv/service/UsuarioService.java
@@ -16,30 +16,30 @@ import net.originmobi.pdv.repository.UsuarioRepository;
 @Service
 public class UsuarioService {
 
-	@Autowired
 	private UsuarioRepository usuarios;
 
-	@Autowired
 	private GrupoUsuarioService grupos;
 
-	LocalDate dataAtual = LocalDate.now();
+    LocalDate dataAtual = LocalDate.now();
 
-	/*
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarios, GrupoUsuarioService grupos) {
+        this.usuarios = usuarios;
+        this.grupos = grupos;
+    }
+
+    /*
 	 * Cadastra o usuário, caso o mesmo já não existe e a pessoa não esteja
 	 * vinculado a outro usuário.
 	 */
 	public String cadastrar(Usuario usuario) {
 		usuario.setData_cadastro(Date.valueOf(dataAtual));
 		String mensagem = "";
-		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		usuario.setSenha(encoder.encode(usuario.getSenha()));
-		
 		if (usuario.getCodigo() == null) {
-
 			Usuario usuarioExiste = usuarios.findByUserEquals(usuario.getUser());
 			Usuario pessoaUsuario = usuarios.findByPessoaCodigoEquals(usuario.getPessoa().getCodigo());
-
 			if (usuarioExiste != null) {
 				return mensagem = "Usuário já existe";
 			} else if (pessoaUsuario != null) {
@@ -49,65 +49,47 @@ public class UsuarioService {
 				mensagem = "Usuário salvo com sucesso";
 			}
 		} else {
-			try {
-				usuarios.save(usuario);
-				mensagem = "Usuário atualizado com sucesso";
-			} catch (Exception e) {
-				mensagem = e.getMessage();
-				e.getStackTrace();
-			}
+			usuarios.save(usuario);
+			mensagem = "Usuário atualizado com sucesso";
 		}
-
 		return mensagem;
 	}
 
-	public List<Usuario> lista() {
-		return usuarios.findAll();
-	}
+    public List<Usuario> lista() {
+        return usuarios.findAll();
+    }
 
 	public String addGrupo(Long codUsu, Long codGru) {
 		Usuario usuario = usuarios.findByCodigoIn(codUsu);
 		GrupoUsuario gruposUsu = grupos.buscaGrupo(codGru);
-
 		List<GrupoUsuario> listaGrupo = new ArrayList<>();
 		listaGrupo.add(gruposUsu);
-
 		if (!usuario.getGrupoUsuario().contains(gruposUsu)) {
 			usuario.getGrupoUsuario().add(gruposUsu);
 		} else {
 			return "ja existe";
 		}
-
 		usuarios.save(usuario);
-
 		return "ok";
 	}
 
 	public String removeGrupo(Long codUsu, Long codGru) {
 		Usuario usuario = usuarios.findByCodigoIn(codUsu);
 		GrupoUsuario gruposUsu = grupos.buscaGrupo(codGru);
-
-		List<GrupoUsuario> todosGrupos = new ArrayList<>();
+		List<GrupoUsuario> todosGrupos;
 		todosGrupos = grupos.buscaGrupos(usuario);
-
 		for (int i = 0; i < todosGrupos.size(); i++) {
 			if (todosGrupos.get(i).getCodigo() == gruposUsu.getCodigo()) {
 				todosGrupos.remove(i);
 			}
 		}
-
-		try {
-			usuario.setGrupoUsuario(todosGrupos);
-			usuarios.save(usuario);
-		} catch (Exception e) {
-			e.getStackTrace();
-		}
-
+		usuario.setGrupoUsuario(todosGrupos);
+		usuarios.save(usuario);
 		return "ok";
 	}
 
-	public Usuario buscaUsuario(String username) {
-		return usuarios.findByUserEquals(username);
-	}
-	
+    public Usuario buscaUsuario(String username) {
+        return usuarios.findByUserEquals(username);
+    }
+
 }
